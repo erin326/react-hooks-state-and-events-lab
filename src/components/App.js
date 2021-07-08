cb0ece30a2f7de733497345119edcd9549146382
erin <erinverkoulen93@gmail.com>
2021-07-08 12:23:47 -0500

    commit tree 6fefbb9b5858bed9619490d92d228cb1719ce148

--- src/components/App.js
+++ src/components/App.js
@@ -4,21 +4,25 @@ import itemData from "../data/items";
 
 function App() {
   const [items, setItems] = useState(itemData);
+  const [isLightOn, setLight] = useState("App light");
 
   // this data will be passed down to the ShoppingList as a prop
-  console.log(items);
+  // console.log(items);
+  function handleDarkModeClick() {
+  setLight(isLightOn => !isLightOn);
+  }
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const appClass = isLightOn ? "App light" : "App dark"
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleDarkModeClick}>Dark Mode</button>
       </header>
-      <ShoppingList items={items} />
+      <ShoppingList items={items} setItems={setItems}/>
     </div>
   );
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [isInCart, setCart] = useState("");
+
+  function handleAddToCartClick() {
+    setCart(isInCart => !isInCart);
+  }
+
+  const appClass = isInCart ? "in-cart" : ""
+
+
   return (
-    <li className="">
+    <li className={appClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleAddToCartClick}  className="add">Add to Cart</button>
     </li>
   );
 }
